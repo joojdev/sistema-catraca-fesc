0d3d9c0e6a7f4a56af4858a34832a54c841dd00b
joojdev <jvpiovezan2905@gmail.com>
2025-06-30 18:43:13 +0000

    Chores: CatracaClient organizado

--- src/CatracaClient.ts
+++ src/CatracaClient.ts
@@ -1,24 +1,27 @@
 import TCPClient from './TCPClient';
 
+type Result<T> = { ok: true, value: T } | { ok: false, error: Error };
+
 class CatracaClient {
     private tcpClient: TCPClient | null = null;
 
-    constructor(private ip: string, private port: number, private release_time: number) {
+    constructor(private ip: string, private port: number, private release_time: number = 40) {
         this.tcpClient = new TCPClient(this.ip, this.port);
+        this.tcpClient.connect();
     }
 
-    private checksum(buffer: Buffer): number {
+    private _checksum(buffer: Buffer): number {
         let sum = 0;
-        for (const byte of buffer) sum ^= byte;
+        for (const byte of buffer) sum ^= byte; // XOR
         return sum;
     }
 
-    private async send(index: number, payload: string): Promise<false | string> {
+    private _buildFrame(index: number, payload: string): Buffer {
         const data = Buffer.from(`${index}+${payload}`, 'ascii');
         const size = Buffer.from([data.length, 0]);
-        const checksum = Buffer.from([this.checksum(Buffer.concat([data, size]))]);
+        const checksum = Buffer.from([this._checksum(Buffer.concat([data, size]))]);
 
-        const fullPayload = Buffer.concat([
+        const builtFrame = Buffer.concat([
             Buffer.from([0x02]),
             size,
             data,
@@ -26,7 +29,15 @@ class CatracaClient {
             Buffer.from([0x03])
         ]);
 
-        return await this.tcpClient!.send(fullPayload);
+        return builtFrame;
+    }
+
+    private async _send(index: number, payload: string): Promise<Result<string>> {
+        const frame = this._buildFrame(index, payload);
+        const error = await this.tcpClient!.send(frame);
+
+        if (!error) return { ok: true, value: 'success!' };
+        return { ok: false, error: new Error(error) };
     }
 }
 
