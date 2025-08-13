decdc6cae9c2e3a733fa8c47f26206695b4af774
joojdev <jvpiovezan2905@gmail.com>
2025-08-13 15:21:29 -0300

    Fix: database setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Apply migrations (creates SQLite DB if missing)
-npx prisma migrate deploy
+npx prisma db push
 
 # Start both scripts with PM2
 npx pm2-runtime start ecosystem.config.js
